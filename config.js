268bb41d96a937e46266b3ca87abafb88da02bdd
Nitin kumar <nitin221416@gmail.com>
2026-03-22 22:09:35 +0530

    firebase

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBrd9cydFHNeJdZNsKU6cYbpPUMukuzncE",
   authDomain: "synczone-f503a.firebaseapp.com",
   projectId: "synczone-f503a",
   storageBucket: "synczone-f503a.firebasestorage.app",
   messagingSenderId: "509144779369",
   appId: "1:509144779369:web:f0e68995dc0a2b120b30f8",
-  // Add the line below
   databaseURL: "https://synczone-f503a-default-rtdb.asia-southeast1.firebasedatabase.app" 
 };
